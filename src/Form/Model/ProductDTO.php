d11d0e87386bc1a339bb99650864902527af44db
josealberola <josealberolaat@gmail.com>
2023-07-28 19:27:41 +0200

    files unfollowed

--- src/Form/Model/ProductDTO.php
+++ src/Form/Model/ProductDTO.php
@@ -3,10 +3,13 @@
 namespace App\Form\Model;
 
 use App\Entity\Product;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ProductDTO {
     public $ean = [];
+    #[Assert\NotBlank]
     public $name;
+    #[Assert\NotBlank]
     public $price;
     public $category;
     public $manufacturer;
